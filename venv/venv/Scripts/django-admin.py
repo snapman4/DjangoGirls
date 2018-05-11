bf06507cf53f119c80d016ae1d357ac33c76f8d4
snapman4 <donaldpaulmunson@gmail.com>
2018-05-11 11:39:30 -0700

    Django girls

--- venv/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\donal\desktop\projects\djangogirls\venv\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
