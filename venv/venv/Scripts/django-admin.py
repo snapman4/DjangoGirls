c89882be7b719cf727004cf74a1d4b91f7477866
snapman4 <donaldpaulmunson@gmail.com>
2018-05-11 11:14:53 -0700

    My Django Girls app first commit

---
+++ venv/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\donal\desktop\projects\djangogirls\venv\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
